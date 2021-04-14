011eed3ac281d7da3b0dea56563e6073f2dadcb1
Alexander L Khlybov <alexander.l.khlybov@gmail.com>
2021-04-14 13:16:56 +0300

    base impl SCID

--- src/input_data_processing/SuperCheckInputData.java
+++ src/input_data_processing/SuperCheckInputData.java
@@ -1,4 +1,43 @@
 package input_data_processing;
 
+import primitives.SuperAllowedCharacters;
+
 public class SuperCheckInputData {
+    public static void checkInputData (String inputData) throws Exception {
+
+        // проверка на наличие иных символов
+        for (int i = 0; i < inputData.length(); i++){
+            if(!SuperAllowedCharacters.isAllowedCharacter(inputData.charAt(i)))
+                throw new Exception("Incorrect input data.");
+        }
+
+        // проверка на корректность наличия символа операции
+        int tmp = 0;
+        for (int i = 0; i < inputData.length(); i++){
+            if(SuperAllowedCharacters.isOperation(inputData.charAt(i)))
+                tmp++;
+        }
+        if (tmp == 0)
+            throw new Exception("Operation symbol not specified.");
+        if (tmp != 1)
+            throw new Exception("Too many op symbols.");
+
+        // проверка на местоположение символа операции
+        if (SuperAllowedCharacters.isOperation(inputData.charAt(0)) &&
+                (inputData.charAt(0) != '-') && (inputData.charAt(0) != '('))
+            throw new Exception("The operation character cannot be at the beginning of the line.");
+        if (SuperAllowedCharacters.isOperation((inputData.charAt(inputData.length() - 1))))
+            throw new Exception("The operation character cannot be at the end of the line");
+
+        // проверка на наличие римских и арабских цифр в строке одновременно
+        boolean arabic = false, roman = false;
+        for (int i = 0; i < inputData.length(); i++){
+            if (SuperAllowedCharacters.isArabic(inputData.charAt(i)))
+                arabic = true;
+            else if (SuperAllowedCharacters.isRoman(inputData.charAt(i)))
+                roman = true;
+        }
+        if (arabic && roman)
+            throw new Exception("Arabic and Roman numerals cannot be on the same line.");
+    }
 }

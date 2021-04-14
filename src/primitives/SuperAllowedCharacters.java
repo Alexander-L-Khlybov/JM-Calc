393474eb88bce4103d9e70cf4d8a0f5ca035a1f6
Alexander L Khlybov <alexander.l.khlybov@gmail.com>
2021-04-14 13:10:07 +0300

    SuperAllowedCharacters impl

--- src/primitives/SuperAllowedCharacters.java
+++ src/primitives/SuperAllowedCharacters.java
@@ -1,4 +1,43 @@
 package primitives;
 
+import java.util.HashSet;
+
 public class SuperAllowedCharacters {
+    private static final HashSet<Character> ARABIC_OPERANDS;
+    static {
+        ARABIC_OPERANDS = new HashSet<>();
+
+        ARABIC_OPERANDS.add(',');
+        ARABIC_OPERANDS.add('.');
+    }
+
+    private static final HashSet<Character> OPERATION;
+    static {
+        OPERATION = new HashSet<>();
+
+        OPERATION.add('(');
+        OPERATION.add(')');
+    }
+
+    public static boolean isOperation (Character op){
+
+        return AllowedCharacters.isOperation(op) || OPERATION.contains(op);
+    }
+
+    public static boolean isArabic (Character ar){
+
+        return AllowedCharacters.isArabic(ar) || ARABIC_OPERANDS.contains(ar);
+    }
+
+    public static boolean isRoman (Character ro){
+
+        return AllowedCharacters.isRoman(ro);
+    }
+
+    public static boolean isAllowedCharacter (Character ac){
+
+        return isOperation(ac) || isArabic(ac) || isRoman(ac);
+    }
+
+
 }
